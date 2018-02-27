eb8edba3f67b7f5080593fea5705e1acc93b3a06
Sebastian Vanegas <svanegas@appfuture.net>
2018-02-27 08:03:08 -0500

    Se sube ejemplos de componentes, directivas y binding

---
+++ zoo/src/components/Animal.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <span class="animal" v-html="icon" @click="toggleDescription"></span>
+    <span class="description" v-show="showDescription">{{description}}</span>
+  </li>
+</template>
+<script>
+  export default {
+    props:['animal', 'description', 'icon'],
+    data(){
+      return {
+        showDescription:false
+      }
+    },
+    methods:{
+      toggleDescription(){
+          this.showDescription = !this.showDescription
+      }
+    }
+  }
+</script>
+<style>
+  .animal{
+    cursor: pointer;
+  }
+</style>
